96417a65086b7835242e4a5e2c19fb4212b5adfd
Sagar Khade <sagar.khade_24uds@sanjivani.edu.in>
2024-12-05 14:41:13 +0530

    hello.java

--- hello.java
+++ hello.java
@@ -1,6 +1,6 @@
 class hello{
   public static void main(String[]args){
 System.out.println("Hello World");
-    System.out.println("Sagar Bhau Khade");
+    System.out.println("welcome to Sinnar");
   }
 }
